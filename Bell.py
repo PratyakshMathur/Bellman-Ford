0b8c6708af132bffc64162798084f95d62372c8e
Pratyaksh <50330619+PratyakshMathur@users.noreply.github.com>
2020-08-25 15:38:17 +0530

    Create Bell.py

---
+++ Bell.py
@@ -0,0 +1,35 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def _AddEdge(self, u, v, w):
+        self.graph.append([u, v, w])
+        print(self.graph)
+    def _PrintGraph(self, dist):
+        print("Vertex : Distance from Source")
+        for i in range(self.V):
+            print("%d \t\t %d " % (i, dist[i]))
+
+    def BellmanFord(self, src):
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                print("Graph contains negative cycle!!")
+                return
+        self.printGraph(dist)
+
+
+vertices = int(input("Enter number of vertices:\n"))
+edges = int(input("Enter number of edges:\n"))
+g = Graph(vertices)
+for i in range(edges):
+    u = int(input("Source vertex:\n"))
+    v = int(input("Destination vertex:\n"))
+    w = int(input("weight of Edge:\n"))
+    g._AddEdge(u, v, w)
+g.BellmanFord(0)
